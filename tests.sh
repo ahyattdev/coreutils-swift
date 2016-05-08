d336f186707ab3842fc85c73dce2b1d3709f4e32
Andrew Hyatt <ahyattdev@icloud.com>
2016-05-08 14:54:03 -0700

    Added tests.sh

---
+++ tests.sh
@@ -0,0 +1,15 @@
+# ls
+echo
+$BUILD_BIN_ROOT/ls tests.sh
+echo
+$BUILD_BIN_ROOT/ls
+echo
+$BUILD_BIN_ROOT/ls src lib
+echo
+$BUILD_BIN_ROOT/ls -a
+echo
+$BUILD_BIN_ROOT/ls -A
+echo
+$BUILD_BIN_ROOT/ls --help
+echo
+
