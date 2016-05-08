2459fd6b7183d9f64fc85de49168638c648176aa
Andrew Hyatt <ahyattdev@icloud.com>
2016-05-07 20:34:52 -0700

    Added code to check if the provided paths exist

--- src/ls/main.swift
+++ src/ls/main.swift
@@ -1,7 +1,34 @@
 import Foundation
+import CommandLine
+
+//#if os(OSX)
+//    import Darwin
+//#elseif os(Linux)
+//    import Glibc
+//#endif
 
 var cli = CLI()
 
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Display this help and exit")
+
+cli.addOptions(help)
+
 cli.safeParse()
 
-print("lets get this started")
+if help.value {
+    cli.printUsage()
+    exit(0)
+}
+
+let fm = NSFileManager.defaultManager()
+let listPaths = cli.unparsedArguments
+
+for path in listPaths {
+    guard fm.fileExistsAtPath(path) else {
+        print("ls: no such path \(path)")
+        exit(1)
+    }
+}
+
+exit(0)
