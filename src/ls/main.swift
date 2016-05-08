316afd4a6fcc45f5392de95e5a0df941f581cd30
Andrew Hyatt <ahyattdev@icloud.com>
2016-05-08 13:40:13 -0700

    Added the basic functionality of ls

--- src/ls/main.swift
+++ src/ls/main.swift
@@ -7,27 +7,94 @@ import CommandLine
 //    import Glibc
 //#endif
 
+let fm = NSFileManager.defaultManager()
+
+enum Mode {
+    case Auto
+    case MultiColumn
+    case Details
+}
+
 var cli = CLI()
 
-let help = BoolOption(shortFlag: "h", longFlag: "help",
+let showDotFiles = BoolOption(shortFlag: "a", longFlag: "all",
+                              helpMessage: "Do not ignore dotfiles")
+
+let showDotFilesWithoutImplied = BoolOption(shortFlag: "A", longFlag: "almost-all",
+                              helpMessage: "Do not ignore dotfiles, excluding the implied . and ..")
+
+let humanReadable = BoolOption(shortFlag: "h", longFlag: "human-readable",
+                               helpMessage: "Print human readable sizes")
+let showColumns = BoolOption(shortFlag: "C", longFlag: "columns", helpMessage: "Use the columned listing format")
+
+let showDetails = BoolOption(shortFlag: "l", longFlag: "details", helpMessage: "Use the detailed listing format")
+
+let help = BoolOption(shortFlag: "H", longFlag: "help",
                       helpMessage: "Display this help and exit")
 
-cli.addOptions(help)
+
+cli.addOptions(showDotFiles, showDotFilesWithoutImplied, humanReadable, showColumns, showDetails, help)
 
 cli.safeParse()
+
+func columnLabel(path: String) -> String {
+    return fm.displayNameAtPath(path)
+}
+
+func list(path: String) {
+    do {
+        let contents = try fm.contentsOfDirectoryAtPath(path)
+            
+        for file in contents {
+            print(columnLabel(file) + "\t")
+        }
+    } catch {
+        print("error getting contents of directory")
+        exit(2)
+    }
+}
+
+
+
+
+var mode : Mode
 
 if help.value {
     cli.printUsage()
     exit(0)
 }
 
-let fm = NSFileManager.defaultManager()
-let listPaths = cli.unparsedArguments
+if showColumns.value {
+    mode = .MultiColumn
+} else if showDetails.value {
+    mode = .Details
+} else {
+    mode = .Auto
+}
+
+var listPaths = cli.unparsedArguments
+
+if listPaths.count == 0 {
+    listPaths.append(".")
+}
 
 for path in listPaths {
     guard fm.fileExistsAtPath(path) else {
         print("ls: no such path \(path)")
         exit(1)
+    }
+}
+
+for path in listPaths {
+    var isDirectory: ObjCBool = false
+    fm.fileExistsAtPath(path, isDirectory: &isDirectory)
+    if isDirectory {
+        if listPaths.count > 1 {
+            print(fm.displayNameAtPath(path))
+        }
+        list(path)
+    } else {
+        print(columnLabel(path))
     }
 }
 
