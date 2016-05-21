f6c403d08da7654ecbaf61a60b8637782bef17a4
Andrew Hyatt <ahyattdev@icloud.com>
2016-05-21 15:22:15 -0700

    Added help text to true and false

---
+++ src/pwd/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  coreutils-swift
+//
+//  Created by Andrew Hyatt on 5/21/16.
+//  Copyright © 2016 Andrew Hyatt. All rights reserved.
+//
+
+import Foundation
--- src/true/main.swift
+++ src/true/main.swift
@@ -1,7 +1,44 @@
+import CommandLine
+import Rainbow
+
 #if os(OSX) || os(iOS)
     import Darwin
 #elseif os(Linux)
     import Glibc
 #endif
 
-exit(EXIT_SUCCESS)
+let cli = CommandLine()
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .About:
+        str = s.lightCyan + "\n\nExits with a status of 0.".yellow
+    case .Error:
+        str = s.red.bold
+    case .OptionFlag:
+        str = s.green.underline
+    case .OptionHelp:
+        str = s.lightBlue
+    }
+    
+    return cli.defaultFormat(str, type: type)
+}
+
+let help = BoolOption(shortFlag: "h", longFlag: "help", helpMessage: "Display this help and exit")
+
+cli.addOptions(help)
+
+do {
+    try cli.parse(true)
+} catch {
+    cli.printUsage(error)
+    exit(EXIT_FAILURE)
+}
+
+if help.value {
+    cli.printUsage()
+    exit(EXIT_SUCCESS)
+} else {
+    exit(EXIT_SUCCESS)
+}
